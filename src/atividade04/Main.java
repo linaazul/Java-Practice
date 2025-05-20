d70c139f083c8390239018dd3fbbab215f762fd5
Ana Guimarães <ana.carolina.gui.ramos@gmail.com>
2025-05-20 16:07:10 -0300

    Add class main

---
+++ src/atividade04/Main.java
@@ -0,0 +1,11 @@
+package atividade04;
+
+public class Main {
+    public static void main(String[] args) {
+        missingCharClass missing = new missingCharClass();
+        String teste = missing.missingChar("teste", 2);
+
+        System.out.println(teste);
+
+    }
+}
